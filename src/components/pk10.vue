011d2ce65468a0974c653dce80196dd5639409a0
shady <shady@zy.com>
2018-03-13 18:15:31 +0800

    pk10也可以下注啦

--- src/components/pk10.vue
+++ src/components/pk10.vue
@@ -93,7 +93,7 @@
             <a href="">1000</a>
             <a href="">设置快速金额</a>
             <a class="pull-right chongtian" @click="clear_bet">重填</a>
-            <a href="" class="pull-right tijiao">提交</a>
+            <a @click="comfire_content" class="pull-right tijiao">提交</a>
           </div>
           <div class="clear"></div>
         </div>
@@ -138,7 +138,7 @@
               <div v-for="(val,key,idx) in singleball_a" class="first-ball-details">
                 <span :class="'hao' + (key+1)">{{key+1}}</span>
                 <span class="he22">{{v[val]}}</span>
-                <input type="text" class="innnn" v-model="bets.single_ball['ball_'+ (index+1) +'_digit'][key]">
+                <input type="text" class="innnn" v-model="bets.single_ball['ball_'+ (index+1) +'_digit'][key]"  @click="single_ball_1_10(k,key)">
                 <div class="clear"></div>
               </div>
               <div class="first-ball-top">
@@ -161,7 +161,7 @@
               <div v-for="(v,index) in sum_digit_a" class="long-bet-content">
                 <span>{{index+3}}</span>
                 <span>{{odds.sum_digit[v]}}</span>
-                <input type="text" v-model="bets.sum_digit[index]">
+                <input type="text" v-model="bets.sum_digit[index]" @click="sumbo(v,index)">
                 <div class="clear"></div>
               </div>
               <div class="clear"></div>
@@ -174,7 +174,7 @@
               <div v-for="(v,index) in sum_half_a" class="long-bet-content">
                 <span>{{sum_half_b[index]}}</span>
                 <span>{{odds.sum_half[v]}}</span>
-                <input type="text" v-model="bets.sum_half[index]">
+                <input type="text" v-model="bets.sum_half[index]" @click="bet_db2(v,index)" >
                 <div class="clear"></div>
               </div>
 
@@ -189,7 +189,7 @@
               <div v-for="(v,index) in singleball_a" class="long-bet-content ">
                 <span :class="'hao'+(index+1)">{{index+1}}</span>
                 <span>{{odds.single_ball.ball_1_digit[v]}}</span>
-                <input type="text" v-model="bets.single_ball.ball_1_digit[index]">
+                <input type="text" v-model="bets.single_ball.ball_1_digit[index]" @click="single_ball_1(v,index,'ball_1_digit')">
                 <div class="clear"></div>
               </div>
 
@@ -199,7 +199,7 @@
               <div v-for="(v,index) in double_aspect_a" class="long-bet-content">
                 <span class="font16px">{{double_aspect_c[index]}}</span>
                 <span>{{odds.double_aspect.ball_1_half[v]}}</span>
-                <input type="text" v-model="bets.double_aspect.ball_1_half[index]">
+                <input type="text" v-model="bets.double_aspect.ball_1_half[index]" @click="single_ball_2(v,index,'ball_1_half')">
                 <div class="clear"></div>
               </div>
 
@@ -214,7 +214,7 @@
               <div v-for="(v,index) in singleball_a" class="long-bet-content ">
                 <span :class="'hao'+(index+1)">{{index+1}}</span>
                 <span>{{odds.single_ball.ball_2_digit[v]}}</span>
-                <input type="text" v-model="bets.single_ball.ball_2_digit[index]">
+                <input type="text" v-model="bets.single_ball.ball_2_digit[index]" @click="single_ball_1(v,index,'ball_2_digit')">
                 <div class="clear"></div>
               </div>
               <div class="clear"></div>
@@ -223,7 +223,7 @@
               <div v-for="(v,index) in double_aspect_a" class="long-bet-content">
                 <span class="font16px">{{double_aspect_c[index]}}</span>
                 <span>{{odds.double_aspect.ball_2_half[v]}}</span>
-                <input type="text" v-model="bets.double_aspect.ball_2_half[index]">
+                <input type="text" v-model="bets.double_aspect.ball_2_half[index]" @click="single_ball_2(v,index,'ball_2_half')">
                 <div class="clear"></div>
               </div>
               <div class="clear"></div>
@@ -244,7 +244,7 @@
               <div v-for="(val,key) in singleball_a" class="long-bet-content ">
                 <span :class="'hao'+(key+1)">{{key+1}}</span>
                 <span>{{odds.single_ball[k][val]}}</span>
-                <input type="text" v-model="bets.single_ball[k][key]">
+                <input type="text" v-model="bets.single_ball[k][key]" @click="single_ball_1(val,key,k)">
                 <div class="clear"></div>
               </div>
 
@@ -254,7 +254,7 @@
               <div v-for="(val,key) in double_aspect_a" class="long-bet-content">
                 <span class="font16px">{{double_aspect_c[key]}}</span>
                 <span>{{odds.double_aspect['ball_' + (index+1) + '_half'][val]}}</span>
-                <input type="text" v-model="bets.double_aspect['ball_' + (index+1) + '_half'][key]">
+                <input type="text" v-model="bets.double_aspect['ball_' + (index+1) + '_half'][key]" @click="single_ball_2(val,key,k)">
                 <div class="clear"></div>
               </div>
 
@@ -276,7 +276,7 @@
               <div v-for="(val,key) in singleball_a" class="long-bet-content ">
                 <span :class="'hao'+(key+1)">{{key+1}}</span>
                 <span>{{odds.single_ball[k][val]}}</span>
-                <input type="text" v-model="bets.single_ball[k][key]">
+                <input type="text" v-model="bets.single_ball[k][key]" @click="single_ball_1(val,key,k)">
                 <div class="clear"></div>
               </div>
 
@@ -286,7 +286,7 @@
               <div v-for="(val,key) in double_aspect_a" v-if="odds.double_aspect['ball_' + (index+1) + '_half'][val]" class="long-bet-content">
                 <span class="font16px">{{double_aspect_c[key]}}</span>
                 <span>{{odds.double_aspect['ball_' + (index+1) + '_half'][val]}}</span>
-                <input type="text" v-model="bets.double_aspect['ball_' + (index+1) + '_half'][key]">
+                <input type="text" v-model="bets.double_aspect['ball_' + (index+1) + '_half'][key]" @click="single_ball_2(val,key,k)">
                 <div class="clear"></div>
               </div>
 
@@ -310,7 +310,7 @@
             <a href="">1000</a>
             <a href="">设置快速金额</a>
             <a  class="pull-right chongtian" @click="clear_bet">重填</a>
-            <a href="" class="pull-right tijiao">提交</a>
+            <a @click="comfire_content" class="pull-right tijiao">提交</a>
           </div>
           <div class="clear"></div>
         </div>
@@ -528,7 +528,7 @@
           sum_half_a:['A','B','C','D'],
           sum_half_b:['冠军单','冠军双','冠亚大','冠亚小'],
 
-
+          //赔率
           odds:{
             single_ball:{
               'ball_1_digit':{'A':0,},
@@ -557,6 +557,7 @@
             sum_digit:{},
             sum_half:{},
           },
+          //下注金额
           bets:{
             single_ball:{
               'ball_1_digit':['','','','','','','','','','',],
@@ -586,11 +587,81 @@
             sum_half:['','','',''],
           },
           bet_content:[],//下注内容的集合
+          //参照表1
+          dicrationaries:[
+            //两面
+            'ball_1_half__K','ball_1_half__L','ball_1_half__M','ball_1_half__N','ball_1_half__O','ball_1_half__P',
+            'ball_2_half__K','ball_2_half__L','ball_2_half__M','ball_2_half__N','ball_2_half__O','ball_2_half__P',
+            'ball_3_half__K','ball_3_half__L','ball_3_half__M','ball_3_half__N','ball_3_half__O','ball_3_half__P',
+            'ball_4_half__K','ball_4_half__L','ball_4_half__M','ball_4_half__N','ball_4_half__O','ball_4_half__P',
+            'ball_5_half__K','ball_5_half__L','ball_5_half__M','ball_5_half__N','ball_5_half__O','ball_5_half__P',
+            'ball_6_half__K','ball_6_half__L','ball_6_half__M','ball_6_half__N',
+            'ball_7_half__K','ball_7_half__L','ball_7_half__M','ball_7_half__N',
+            'ball_8_half__K','ball_8_half__L','ball_8_half__M','ball_8_half__N',
+            'ball_9_half__K','ball_9_half__L','ball_9_half__M','ball_9_half__N',
+            'ball_10_half__K','ball_10_half__L','ball_10_half__M','ball_10_half__N',
+             //特码
+            'ball_1_digit__A','ball_1_digit__B','ball_1_digit__C',
+            'ball_1_digit__D','ball_1_digit__E','ball_1_digit__F',
+            'ball_1_digit__G','ball_1_digit__H','ball_1_digit__I','ball_1_digit__J',
+            'ball_2_digit__A','ball_2_digit__B','ball_2_digit__C',
+            'ball_2_digit__D','ball_2_digit__E','ball_2_digit__F',
+            'ball_2_digit__G','ball_2_digit__H','ball_2_digit__I','ball_2_digit__J',
+            'ball_3_digit__A','ball_3_digit__B','ball_3_digit__C',
+            'ball_3_digit__D','ball_3_digit__E','ball_3_digit__F',
+            'ball_3_digit__G','ball_3_digit__H','ball_3_digit__I','ball_3_digit__J',
+            'ball_4_digit__A','ball_4_digit__B','ball_4_digit__C',
+            'ball_4_digit__D','ball_4_digit__E','ball_4_digit__F',
+            'ball_4_digit__G','ball_4_digit__H','ball_4_digit__I','ball_4_digit__J',
+            'ball_5_digit__A','ball_5_digit__B','ball_5_digit__C',
+            'ball_5_digit__D','ball_5_digit__E','ball_5_digit__F',
+            'ball_5_digit__G','ball_5_digit__H','ball_5_digit__I','ball_5_digit__J',
+
+             //冠亚军总和
+            'sum_half__A','sum_half__B','sum_half__C','sum_half__D',
+            'sum_digit__A','sum_digit__B','sum_digit__C','sum_digit__D', 'sum_digit__E',
+            'sum_digit__F','sum_digit__G','sum_digit__H','sum_digit__I',
+            'sum_digit__J','sum_digit__K','sum_digit__L','sum_digit__M','sum_digit__N',
+            'sum_digit__O','sum_digit__P','sum_digit__Q'
+          ],
+          //参照表2
+          dicrationaries_2:[
+            //两面
+            '第一名大','第一名小','第一名单','第一名双','第一名龙','第一名虎',
+            '第二名大','第二名小','第二名单','第二名双','第二名龙','第二名虎',
+            '第三名大','第三名小','第三名单','第三名双','第三名龙','第三名虎',
+            '第四名大','第四名小','第四名单','第四名双','第四名龙','第四名虎',
+            '第五名大','第五名小','第五名单','第五名双','第五名龙','第五名虎',
+            '第六名大','第六名小','第六名单','第六名双',
+            '第七名大','第七名小','第七名单','第七名双',
+            '第八名大','第八名小','第八名单','第八名双',
+            '第九名大','第九名小','第九名单','第九名双',
+            '第十名大','第十名小','第十名单','第十名双',
+            //特码
+            '第一名-特码-1','第一名-特码-2','第一名-特码-3','第一名-特码-4',
+            '第一名-特码-5','第一名-特码-6','第一名-特码-7','第一名-特码-8','第一名-特码-9','第一名-特码-10',
+            '第二名-特码-1','第二名-特码-2','第二名-特码-3','第二名-特码-4',
+            '第二名-特码-5','第二名-特码-6','第二名-特码-7','第二名-特码-8','第二名-特码-9','第二名-特码-10',
+            '第三名-特码-1','第三名-特码-2','第三名-特码-3','第三名-特码-4',
+            '第三名-特码-5','第三名-特码-6','第三名-特码-7','第三名-特码-8','第三名-特码-9','第三名-特码-10',
+            '第四名-特码-1','第四名-特码-2','第四名-特码-3','第四名-特码-4',
+            '第四名-特码-5','第四名-特码-6','第四名-特码-7','第四名-特码-8','第四名-特码-9','第四名-特码-10',
+            '第五名-特码-1','第五名-特码-2','第五名-特码-3','第五名-特码-4',
+            '第五名-特码-5','第五名-特码-6','第五名-特码-7','第五名-特码-8','第五名-特码-9','第五名-特码-10',
+             //冠亚军总和
+            '冠亚军-大','冠亚军-小','冠亚军-单','冠亚军-双',
+            '冠亚军-特码-3','冠亚军-特码-4','冠亚军-特码-5','冠亚军-特码-6','冠亚军-特码-7',
+            '冠亚军-特码-8','冠亚军-特码-9','冠亚军-特码-10','冠亚军-特码-11','冠亚军-特码-12',
+            '冠亚军-特码-13','冠亚军-特码-14','冠亚军-特码-15','冠亚军-特码-16','冠亚军-特码-17',
+            '冠亚军-特码-18','冠亚军-特码-19',
+
+          ],
 
         };
       return my_data;
     },
-    methods: {
+    methods:
+    {
       showType: function (idx) {
         this.showArray_cqssc = [0, 0, 0, 0, 0, 0, 0];
         this.showArray_cqssc[idx] = 1;
@@ -709,7 +780,6 @@
         this.bets.sum_half.reverse().reverse();//触发视图层改变
         let content = 'sum_half__' + v;
         this.bet_content.push({content:content,money:this.fast_money});//添加到下注内容区
-        console.log(this.bet_content);
       },
       clear_bet:function () {
         //清空数据
@@ -747,7 +817,99 @@
         //更新视图层
         this.bets.single_ball.ball_1_digit.reverse().reverse();
       },
+      single_ball_1_10:function(k,k2){
+        let keys = ['A','B','C','D','E','F','G','H','I','J'];
+        this.bets.single_ball[k][k2] = this.fast_money;//改变下注金额
+        this.bets.single_ball[k].reverse().reverse();//触发视图层改变
+        let content = k + '__' + keys[k2];
+        this.bet_content.push({content:content,money:this.fast_money});//添加到下注内容区
+        console.log(this.bet_content);
+      },
+      sumbo:function(v,k){
+        this.bets.sum_digit[k] = this.fast_money;//改变下注金额
+        this.bets.sum_digit.reverse().reverse();//触发视图层改变
+        let content = 'sum_digit__' + v;
+        this.bet_content.push({content:content,money:this.fast_money});//添加到下注内容区
+        console.table(this.bet_content)
+      },
+      single_ball_1:function (v,k,str) {
+        this.bets.single_ball[str][k] = this.fast_money;//改变下注金额
+        this.bets.single_ball[str].reverse().reverse();//触发视图层改变
+        let content = str + "__" + v;
+        this.bet_content.push({content:content,money:this.fast_money});//添加到下注内容区
+      },
+      single_ball_2:function (v,k,str) {
+        this.bets.double_aspect[str][k] = this.fast_money;//改变下注金额
+        this.bets.double_aspect[str].reverse().reverse();//触发视图层改变
+        let content = str + "__" + v;
+        this.bet_content.push({content:content,money:this.fast_money});//添加到下注内容区
+        console.log(this.bet_content)
+      },
+      comfire_content:function()
+      {
+        //当用户没有选择下注内容的时候要提示用户选择
+        if(this.bet_content.length < 1){
+          alert('请选择下注内容后再提交');
+          return 0;
+        }
+        //过滤掉相同的对象
+        this.filter_same();
+        let html = '';
+        for(let i = 0; i<this.bet_content.length;i++){
+          var index = this.dicrationaries.indexOf(this.bet_content[i].content);
+          html += this.dicrationaries_2[index] +  '  @ ￥' +  this.bet_content[i].money  +   '\n';
+        }
+        if(confirm(html)){
+          this.do_bet();
 
+        }
+        else {
+          console.log('取消');
+        }
+
+      },
+      filter_same:function () {
+        for(let i = 0; i < this.bet_content.length;i++)
+        {
+          let key =  this.bet_content[i].content;
+          var flag = false;
+          for(let j = i+1;j<this.bet_content.length;j++)
+          {
+            if(key == this.bet_content[j].content)
+            {
+              this.bet_content.splice(i,1);
+              flag = true;
+              break;
+            }
+          }
+          if(flag)
+          {
+            this.filter_same();
+            break;
+          }
+        }
+      },
+      do_bet:function () {
+        this.$http.post(`${this.global.config.API}pk10/order`,{bets:this.bet_content}).then(function(res){
+          if(res.data.status == 200)
+          {
+            //清除下注内容
+            this.clear_bet();
+            //从服务器上获取余额
+            this.$http.get(config.API + "user/" + window.sessionStorage.user_id ).then(function (response)
+            {
+              let  data = response.data.data.user;
+              this.$set(this.$store.state,'cash_money',data.money.cash_money)
+            });
+            alert(res.data.msg);
+          }
+          else
+          {
+            alert(res.data.msg);
+          }
+
+        });
+      },
 
     },
     created: function () {
@@ -780,7 +942,6 @@
 
     },
     mounted:function(){
-
     },
   }
 </script>
