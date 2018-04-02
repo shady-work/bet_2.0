6c4fe13b50c2597cccf6179c6060936645b10beb
shady <shady@zy.com>
2018-04-02 11:48:37 +0800

    完善充值系统

--- src/components/recharge_.vue
+++ src/components/recharge_.vue
@@ -58,6 +58,7 @@
     box-sizing: border-box;
     border:1px solid gray;
     margin-left: 1px;
+    cursor: pointer;
   }
   .right-left
   {
@@ -91,39 +92,136 @@
     margin-left: 2px;
     margin-top: 15px;
   }
+  .input-my
+  {
+    margin-top: 15px;
+  }
+  .input-my>input
+  {
+    height: 30px;
+    width: 248px;
+    border-radius: 3px;
+    border: none;
+    color: #000;
+    text-indent:5px;
+    font-size: 12px;
+  }
+  .input-my>input::-webkit-input-placeholder{
+    color:gray;
+
+  }
+  .input-my>input::-moz-placeholder{   /* Mozilla Firefox 19+ */
+    color:gray;
+  }
+  .input-my>input:-moz-placeholder{    /* Mozilla Firefox 4 to 18 */
+    color:gray;
+  }
+  .input-my>input:-ms-input-placeholder{  /* Internet Explorer 10-11 */
+    color:gray;
+  }
+  .submit-btn
+  {
+    width: 310px;
+    margin-top: 15px;
+    height: 30px;
+    background: #9988ff;
+    color: white;
+    outline: none;
+    border: none;
+  }
+  .tabs .active,.right-left .active
+  {
+    background: rgba(255,255,255,0.5);
+  }
+  .article-context
+  {
+    width: 80%;
+    margin:0 auto;
+    border: 1px solid #fff;
+    box-sizing: border-box;
+    padding: 15px 5px;
+    margin-top: 25px;
+    font-size: 16px;
+    line-height: 22px;
+  }
 </style>
 <template>
-
     <div id="recharge">
+
         <div class="left">
               <h2 class="title">充值帮助</h2>
               <p class="context">
                 尊敬的客户，您好。感谢您使用XXXX平台充值系统尊敬的客户，您好。
                 感谢您使用XXXX平台充值系统尊敬的客户，您好。感谢您使用XXXX平台充值系统尊敬的客户，您好。感谢您使用XXXX平台充值系统
               </p>
-              <div class="article-context" v-html="content">
-              
-              </div>
+              <!--要显示的充值内容-->
+              <div class="article-context mt5" style="text-align: left;" v-html="content"></div>
         </div>
-
 
         <div class="right">
             <ul class="tabs">
-              <li>银行</li>
-              <li>线下</li>
-              <li>第三方</li>
+              <li @click="choose_one(0)" :class="isActive[0]?'active':''">银行</li>
+              <li @click="choose_one(1)" :class="isActive[1]?'active':''">线下</li>
+              <li @click="choose_one(2)" :class="isActive[2]?'active':''">第三方</li>
             </ul>
             <div class="right-left">
                <ul>
-                 <li v-for="(v,k) in articles" @click="get_an_article(v.id)">
+                 <li v-for="(v,k) in articles" @click="get_an_article(v.id,k)" v-if="v.type == condition" :class="isActive2[k]?'active':''">
                     {{v.title}}
                  </li>
                </ul>
             </div>
-            <div class="right-right">
 
+            <!--银行卡充值-->
+            <div class="right-right" v-show="condition==0">
+            <h2 class="mt5">请填写您的汇款信息</h2>
+            <div class="input-my">
+              <span>银行名称</span>
+              <input type="text" v-model="bank_topup.bank_name" placeholder="请输入您汇款时使用的银行卡归属银行">
             </div>
+
+            <div class="input-my">
+              <span>存款姓名</span>
+              <input type="text" v-model="bank_topup.store_name" placeholder="请输入您汇款时使用的银行卡的户名">
+            </div>
+
+            <div class="input-my">
+              <span>存款账号</span>
+              <input type="text" v-model="bank_topup.bank_number" placeholder="请输入您汇款时使用的银行卡卡号">
+            </div>
+
+            <div class="input-my">
+              <span>存款金额</span>
+              <input type="text" v-model="bank_topup.money" placeholder="请输入您此次汇款的金额（10-50000000）">
+            </div>
+
+            <button class="submit-btn" @click="submit_bank()">提交</button>
+          </div>
+
+            <!--线下充值-->
+            <div class="right-right" v-show="condition==1" >
+            <h2 class="mt5">请填写您的汇款信息</h2>
+            <div class="input-my">
+              <span>账号姓名</span>
+              <input type="text" v-model="offline_topup.offline_name" placeholder="微信昵称/支付宝户名">
+            </div>
+
+            <div class="input-my">
+              <span>存款账号</span>
+              <input type="text" v-model="offline_topup.store_number" placeholder="微信号/支付宝账号">
+            </div>
+
+            <div class="input-my">
+              <span>存款金额</span>
+              <input type="text" v-model="offline_topup.money" placeholder="请输入您此次汇款的金额（10-50000000）">
+            </div>
+
+            <button class="submit-btn" @click="submit_offline()">提交</button>
+          </div>
+
+
         </div>
+
     </div>
 </template>
 
@@ -135,11 +233,30 @@
     data:function()
     {
       return{
+        which_one:'',
         articles:[],//所有文章
         bank:[],//银行的文章
         third:[],//第三方的文章
         offline:[],//线下的文章
         content:'',//文章内容
+        condition:0,//  筛选条件
+        isActive:[1,0,0],//是否选中此类型的充值方式
+        isActive2:[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],//是否选中这篇文章
+        bank_topup:
+        {
+          //银行的充值提交信息
+          bank_name:'',//存款的银行名称
+          store_name:'',//存款的户主名
+          bank_number:'',//卡号
+          money:'',//多少钱
+        },
+        offline_topup:
+          {
+            //线下的充值提交信息
+            offline_name:'',
+            store_number:'',
+            money:'',
+          },
       }
     },
     methods:
@@ -152,6 +269,7 @@
          this.$http.get(`${this.global.config.API}articles`)
            .then(function(res)
            {
+
              if(res.data.status == 200)
               {
                 this.articles = res.data.data.articles.list;
@@ -183,28 +301,109 @@
                 this.third.push(this.articles[i]);
               }
           }
+          this.get_an_article(this.bank[0].id,0);
 
       },
       /**
        * 获取某个文章内容
        * @param a_id
        */
-      get_an_article:function(a_id)
+      get_an_article:function(a_id,index)
       {
+
           this.$http.get(`${this.global.config.API}articles/${a_id}`)
             .then(function(res)
             {
-              console.log(res.data);
               if(res.data.status == 200)
               {
                     this.$set(this,'content',res.data.data.article.content);
+                    this.which_one = res.data.data.article.title;
               }
               else
               {
                 alert('文章获取失败');
               }
             });
+          this.isActive2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+          this.isActive2[index] = 1;
 
+
+
+
+      },
+      /**
+       * 切换充值类型
+       * @param index
+       */
+      choose_one:function(index)
+      {
+        //var flag = 0;
+        this.isActive = [0,0,0];
+        this.isActive[index] = 1;
+        this.condition = index;
+        for(let i = 0;i<this.articles.length;i++)
+        {
+          if(this.articles[i].type === index)
+          {
+            this.get_an_article(this.articles[i].id,i);
+            break;
+          }
+
+        }
+
+      },
+      /**
+       * 提交线下充值的信息
+       */
+      submit_offline:function ()
+      {
+         this.$http.post(`${this.global.config.API}topup`,
+         {
+              money:this.offline_topup.money,
+              con:this.which_one  + '充值',
+              tp_mark:'存款账号：' + this.bank_topup.store_number + "\n" + "存款户名：" + this.offline_topup.offline_name,
+         }).then(function(res)
+         {
+           if(res.data.status == 200)
+           {
+              alert(res.data.msg);
+             window.location.reload();
+           }
+           else
+           {
+             alert('充值失败，请联系管理员，或稍后再试。');
+           }
+           return;
+         });
+      },
+      /**
+       * 提交银行充值内容
+       */
+      submit_bank:function()
+      {
+        let data = {
+          money:this.bank_topup.money,
+          con:this.which_one  + '充值',
+          tp_mark:
+          '银行名称：' + this.bank_topup.bank_name + '\n' +
+          '存款账号：' + this.bank_topup.bank_number + "\n"
+          + "存款户名：" + this.bank_topup.store_name,
+        };
+
+        this.$http.post(`${this.global.config.API}topup`,data)
+          .then(function(res)
+          {
+            if(res.data.status == 200)
+            {
+              alert(res.data.msg);
+              window.location.reload();
+            }
+            else
+            {
+              alert('充值失败，请联系管理员，或稍后再试。');
+            }
+            return;
+          });
       }
     },
     created()
