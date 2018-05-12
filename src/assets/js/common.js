d5c4da3b6647d27da20946a770c3f8a7d671154f
shady <shady@zy.com>
2018-05-12 15:04:07 +0800

    。。。。

---
+++ src/assets/js/common.js
@@ -0,0 +1,24 @@
+/**
+ * common.js
+ */
+
+export default
+{
+    config:
+    {
+          API:"http://lty-main.com/",
+           //API:"http://lty-sub.com/",
+           //   API:"/",
+    },
+    log:function(content)
+    {
+        console.log(content);
+    },
+    alt:function(content)
+    {
+        alert(content);
+    }
+}
+
+
+
